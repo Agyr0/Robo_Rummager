c16abfef2e1ecce02e0d59d913a1abc54eb16a6a
Christian Rucki <ruckichristian@hotmail.com>
2023-09-04 23:33:39 -0700

    Rogue Bot Chase Functionality

--- Assets/Scripts/Enemys/RogueBots.cs
+++ Assets/Scripts/Enemys/RogueBots.cs
@@ -1,46 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class RogueBots : MonoBehaviour
 {
-    #region Patrolling
     private NavMeshAgent agent;
-    private float waitTime;
-    
-    [Header("Patrol Settings")]
-    [SerializeField]
-    [Tooltip("Range that the enemy AI will patrol around in.")]
-    private float range;
+
+    public Transform player;
 
     [SerializeField]
-    [Tooltip("Center Point that the enemy AI will patrol around.")]
-    private Transform centerPoint;
+    private LayerMask playerLayerMask;
+
+    #region Patrolling
+    private float patrolWaitTime;
+
+    [Header("Patrol Settings")]
+    [SerializeField]
+    [Tooltip("Range that the Rogue Bot will patrol around in.")]
+    private float patrolRange;
+
+    [SerializeField]
+    [Tooltip("Center Point that the Rogue Bot will patrol around.")]
+    private Transform patrolCenterPoint;
 
     [SerializeField]
     [Tooltip("Minimum distance that can be between new patrol points.")]
     private float minDistFromLastPoint;
 
     [SerializeField]
-    [Tooltip("Minimum time the enemy AI will wait at a location before moving to another.")]
+    [Tooltip("Minimum time the Rogue Bot will wait at a location before moving to another.")]
     private float minWaitTime;
 
     [SerializeField]
-    [Tooltip("Maximum time the enemy AI will wait at a location before moving to another.")]
+    [Tooltip("Maximum time the Rogue Bot will wait at a location before moving to another.")]
     private float maxWaitTime;
+    #endregion
+
+    #region Chasing
+    [SerializeField]
+    private bool playerInSightRange;
+
+    [SerializeField]
+    [Tooltip("Range that the Rogue Bot will detect the player and begin to chase them.")]
+    private float sightRange;
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        RogueBotPatrolling();
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, playerLayerMask);
+
+        if (playerInSightRange)
+            RogueBotChasing();
+        else
+            RogueBotPatrolling();
     }
 
     // Patrolling
@@ -48,21 +70,21 @@ public class RogueBots : MonoBehaviour
     {
         if (agent.remainingDistance <= agent.stoppingDistance) // Check if Rogue Bot has reached position
         {
-            waitTime -= Time.deltaTime;
-            if (waitTime <= 0)
+            patrolWaitTime -= Time.deltaTime;
+            if (patrolWaitTime <= 0)
             {
                 Vector3 point;
-                if (RandomPoint(centerPoint.position, range, out point))
+                if (RandomPoint(patrolCenterPoint.position, patrolRange, out point))
                 {
-                    Debug.DrawRay(point, Vector3.up, Color.red, 5.0f); // Draw the Rogue Bot's next position
+                    Debug.DrawRay(point, Vector3.up, UnityEngine.Color.red, 5.0f); // Draw the Rogue Bot's next position
                     agent.SetDestination(point);
                 }
             }
         }
         else
         {
-            if (waitTime <= 0)
-                waitTime = Random.Range(minWaitTime, maxWaitTime);
+            if (patrolWaitTime <= 0)
+                patrolWaitTime = Random.Range(minWaitTime, maxWaitTime);
         }
 
         bool RandomPoint(Vector3 center, float range, out Vector3 result)
@@ -85,10 +107,20 @@ public class RogueBots : MonoBehaviour
         }
     }
 
+    // Chasing
+    private void RogueBotChasing()
+    {
+        agent.SetDestination(player.position);
+    }
+
     private void OnDrawGizmos()
     {
         // Draw the Patrolling Range
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(centerPoint.position, range);
+        Gizmos.color = UnityEngine.Color.red;
+        Gizmos.DrawWireSphere(patrolCenterPoint.position, patrolRange);
+
+        // Draw the Sight Range
+        Gizmos.color = UnityEngine.Color.blue;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
