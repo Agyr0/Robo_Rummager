9e0b5b44a6e212b8ceb841775449e140fd5bd965
Tucker Holmes <tgholmes@mail.csuchico.edu>
2023-09-21 15:59:32 -0700

    WORKING ON WORKSHOP WORLDSPACE UI

--- Assets/Scripts/LootableObjects/LootBag.cs
+++ Assets/Scripts/LootableObjects/LootBag.cs
@@ -2,88 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class LootBag : MonoBehaviour
-{
-    #if UNITY_EDITOR 
-    [ArrayElementTitle("elementName")] 
-    #endif
-    public List<DropTableItem> _dropTable = new List<DropTableItem>();
-
-    [SerializeField]
-    private GameObject _resourceFullMesh;
-    [SerializeField, Tooltip("Number of times this lootable item can drop something")]
-    private int _maxDrops = 10;
-    private int _dropsLeft;
-
-    [SerializeField, Header("Drops Rigidbody Effects")]
-    private float _explosionForce = 2f;
-    [SerializeField]
-    private float _explosionRadius = 1f;
-    [SerializeField]
-    private float _upwardsModifier = 2f;
-
-    public GameObject ResourceFullMesh
+    [System.Serializable]
+    public class LootBag : MonoBehaviour
     {
-        get 
+#if UNITY_EDITOR
+        [ArrayElementTitle("elementName")]
+#endif
+        public List<DropTableItem> _dropTable = new List<DropTableItem>();
+
+        [SerializeField]
+        private GameObject _resourceFullMesh;
+        [SerializeField, Tooltip("Number of times this lootable item can drop something")]
+        private int _maxDrops = 10;
+        private int _dropsLeft;
+
+        [SerializeField, Header("Drops Rigidbody Effects")]
+        private float _explosionForce = 2f;
+        [SerializeField]
+        private float _explosionRadius = 1f;
+        [SerializeField]
+        private float _upwardsModifier = 2f;
+
+        public GameObject ResourceFullMesh
         {
-            if (DropsLeft <= 0 && _resourceFullMesh != null)
-                _resourceFullMesh.SetActive(false);
-            else if (DropsLeft > 0 && !_resourceFullMesh.activeInHierarchy && _resourceFullMesh != null)
-                _resourceFullMesh.SetActive(true);
-
-            return _resourceFullMesh; 
-        } 
-    }
-    public int MaxDrops
-    { get { return _maxDrops; } set {  _maxDrops = value; } }
-    public int DropsLeft
-    { get { return _dropsLeft; } set { _dropsLeft = value; } }
-
-
-    private void OnEnable()
-    {
-        EventBus.Subscribe(EventType.REFRESH_RESOURCES, RefreshDrops);
-    }
-    private void OnDisable()
-    {
-        EventBus.Unsubscribe(EventType.REFRESH_RESOURCES, RefreshDrops);
-    }
-
-    private void RefreshDrops()
-    {
-        DropsLeft = MaxDrops;
-        if(_resourceFullMesh != null)
-        {
-            GameObject temp = ResourceFullMesh;
-        }
-
-    }
-
-
-
-    public void DropResource(Vector3 hitPosition)
-    {
-        //Pick a random item in the drop table to drop
-        int itemToDrop = Random.Range(0, _dropTable.Count);
-
-        for (int i = 0; i < DropsLeft; i++)
-        {
-            if(_dropTable[itemToDrop].CheckDrop())
+            get
             {
-                GameObject drop = Instantiate(_dropTable[itemToDrop].Resource, hitPosition, Quaternion.identity);
-                Rigidbody rb = drop.GetComponent<Rigidbody>();
-                if (rb != null)
-                {
-                    rb.AddExplosionForce(_explosionForce, hitPosition, _explosionRadius, _upwardsModifier);
-                }
-                    
+                if (DropsLeft <= 0 && _resourceFullMesh != null)
+                    _resourceFullMesh.SetActive(false);
+                else if (DropsLeft > 0 && !_resourceFullMesh.activeInHierarchy && _resourceFullMesh != null)
+                    _resourceFullMesh.SetActive(true);
+
+                return _resourceFullMesh;
             }
-            DropsLeft--;
         }
-        if (_resourceFullMesh != null)
+        public int MaxDrops
+        { get { return _maxDrops; } set { _maxDrops = value; } }
+        public int DropsLeft
+        { get { return _dropsLeft; } set { _dropsLeft = value; } }
+
+
+        private void OnEnable()
         {
-            GameObject temp = ResourceFullMesh;
+            EventBus.Subscribe(EventType.REFRESH_RESOURCES, RefreshDrops);
+        }
+        private void OnDisable()
+        {
+            EventBus.Unsubscribe(EventType.REFRESH_RESOURCES, RefreshDrops);
+        }
+
+        private void RefreshDrops()
+        {
+            DropsLeft = MaxDrops;
+            if (_resourceFullMesh != null)
+            {
+                GameObject temp = ResourceFullMesh;
+            }
+
+        }
+
+
+
+        public void DropResource(Vector3 hitPosition)
+        {
+            //Pick a random item in the drop table to drop
+            int itemToDrop = Random.Range(0, _dropTable.Count);
+
+            for (int i = 0; i < DropsLeft; i++)
+            {
+                if (_dropTable[itemToDrop].CheckDrop())
+                {
+                    GameObject drop = Instantiate(_dropTable[itemToDrop].Resource, hitPosition, Quaternion.identity);
+                    Rigidbody rb = drop.GetComponent<Rigidbody>();
+                    if (rb != null)
+                    {
+                        rb.AddExplosionForce(_explosionForce, hitPosition, _explosionRadius, _upwardsModifier);
+                    }
+
+                }
+                DropsLeft--;
+            }
+            if (_resourceFullMesh != null)
+            {
+                GameObject temp = ResourceFullMesh;
+            }
         }
     }
-}
