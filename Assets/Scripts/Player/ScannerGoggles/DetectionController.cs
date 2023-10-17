c753b456cb6ba96c307439abe1054aa36f91d797
Tucker Holmes <tgholmes@mail.csuchico.edu>
2023-10-16 17:47:21 -0700

    build v0.0.3.0

--- Assets/Scripts/Player/ScannerGoggles/DetectionController.cs
+++ Assets/Scripts/Player/ScannerGoggles/DetectionController.cs
@@ -61,10 +61,10 @@ public class DetectionController : MonoBehaviour
 
                 for (int i = 0; i < hits.Length; i++)
                 {
-                    if (hits[i].GetComponent<ItemGlowController>() == null)
-                        yield break;
+                    if (hits[i].GetComponent<ItemGlowController>() != null)
+                        hits[i].GetComponent<ItemGlowController>().PlayGlow();
+                        
 
-                    hits[i].GetComponent<ItemGlowController>().PlayGlow();
 
                 }
                 yield return null;
